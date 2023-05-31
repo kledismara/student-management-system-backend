0a7866222fd6d36ab641b86952639f01cc733992
kledismara <kledismara99@gmail.com>
2023-05-31 23:44:21 +0200

    Initial Commit. Student Management System.

--- src/main/java/com/student/repository/StudentRepository.java
+++ src/main/java/com/student/repository/StudentRepository.java
@@ -4,4 +4,5 @@ import com.student.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+    ///TODO Create method to deactivae student with query
 }
